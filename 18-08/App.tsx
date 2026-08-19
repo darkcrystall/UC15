a4745b1d8cfe345c492035c8e5720b413ca53c99
Eduarda C. <dossantosec@outlook.com>
2026-08-18 23:02:38 -0300

    feat: exemplo tabs

--- 18-08/App.tsx
+++ 18-08/App.tsx
@@ -1,18 +1,45 @@
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+// icons
+import { Ionicons } from "@expo/vector-icons";
 // cria o objeto de navegação, nesse caso, navegação por pilhas
 const Stack = createNativeStackNavigator();
+// tabs
+const Tab = createBottomTabNavigator();
 export default function App() {
-// container que envolve a tela para navegação
-  return <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home}/>
-      <Stack.Screen name="Profile" component={Profile}/>
-    </Stack.Navigator>
-  </NavigationContainer>;
+  // container que envolve a tela para navegação
+  return (
+    <NavigationContainer>
+      {/* <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
